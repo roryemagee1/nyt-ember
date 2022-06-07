690909ddc7ca6936242cfbd7ab8d506d3ec43e5a
Rory Magee <roryemagee@gmail.com>
2022-06-07 15:40:25 -0600

    Clean up code

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,12 +3,7 @@ import Route from '@ember/routing/route';
 export default class IndexRoute extends Route {
   async model() {
     let response = await fetch('/api/mock.json');
-    // let data = await response.json();
-    // let output = await data.results;
-    // console.log(output);
-    // return output
     let { results } = await response.json()
-    console.log(results);
     return results
   }
 }
