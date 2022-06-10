426791ff548d19a0d3772589ce1b83f1d8cbe4ad
Rory Magee <roryemagee@gmail.com>
2022-06-09 21:11:48 -0600

    Add functioning fetch request

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,8 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
   async model() {
-    let response = await fetch('/api/mock.json');
+    // let response = await fetch('/api/mock.json');
+    let response = await fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=OGvAOaWN79RLeir943VLb8GYddZl7cSh');
     let { results } = await response.json()
     let newResults = results.map((result, index) => {
       return {...result, id: index}
